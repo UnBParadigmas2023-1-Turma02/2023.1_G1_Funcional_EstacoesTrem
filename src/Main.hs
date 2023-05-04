cffa780720da8506ff833c06b64882afb8ea11eb
Pedro Haick <180129147@aluno.unb.br>
2023-05-04 19:37:50 -0300

    Corrigindo menu e chamando função executarFuncao

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ executarFuncao opcao
   | opcao == "1" = listarEstacoes
   | opcao == "2" = printDistanciaDFS
   | opcao == "3" = printCaminhoDFS
+  | opcao == "4" = printDistanciaBFS
   | otherwise = putStrLn "Opção inválida"
 
 main :: IO ()
@@ -18,48 +19,48 @@ main = do
     putStrLn "|_____|___|_| |__,|___|___|___|___|    |_| |_| |___|_|_|_|"
                                                           
     putStrLn "=========================================================="
-    putStrLn "1 - Listar estações (DFS)"
+    putStrLn "1 - Listar estações"
     putStrLn "2 - Distância entre duas estações (DFS)"
     putStrLn "3 - Caminho entre duas estações (DFS)"
     putStrLn "4 - Distância entre duas estações (BFS)"
-    putStrLn "5 - Caminho entre duas estações"
-    putStrLn "5 - Mostrar lista de adjacência"
+    putStrLn "5 - Caminho entre duas estações (BFS)"
     putStrLn "6 - Mostrar lista de adjacência"
-    putStrLn "7 - Sair"
+    -- putStrLn "7 - Sair"
     putStrLn "=========================================================="
     putStr "Selecione uma opção: "
 
-    input <- getLine-- exemplo de entrada do usuário
-    case input of
-      "1" -> do
-        putStrLn ""
-        listarEstacoes
-        putStrLn ""
-        main
-      "2" -> do
-        putStrLn ""
-        printDistanciaDFS
-        putStrLn ""
-        main
-      "3" -> do
-        putStrLn ""
-        printCaminhoDFS
-        putStrLn ""
-        main
-      "4" -> do
-        putStrLn ""
-        printDistanciaBFS 
-        putStrLn ""
-        main
-      "5" -> do
-        putStrLn ""
-        putStrLn ">>>>>Função de caminho entre estações aqui<<<<<\n"
-        putStrLn ""
-        main
-      "6" -> do
-        putStrLn ""
-        putStrLn ">>>>>Função de lista de adjacências aqui<<<<<\n"
-        putStrLn ""
-        main
-      "7" -> do
-        putStrLn ""
+    input <- getLine -- exemplo de entrada do usuário
+    executarFuncao input
+    -- case input of
+    --   "1" -> do
+    --     putStrLn ""
+    --     listarEstacoes
+    --     putStrLn ""
+    --     main
+    --   "2" -> do
+    --     putStrLn ""
+    --     printDistanciaDFS
+    --     putStrLn ""
+    --     main
+    --   "3" -> do
+    --     putStrLn ""
+    --     printCaminhoDFS
+    --     putStrLn ""
+    --     main
+    --   "4" -> do
+    --     putStrLn ""
+    --     printDistanciaBFS 
+    --     putStrLn ""
+    --     main
+    --   "5" -> do
+    --     putStrLn ""
+    --     putStrLn ">>>>>Função de caminho entre estações aqui<<<<<\n"
+    --     putStrLn ""
+    --     main
+    --   "6" -> do
+    --     putStrLn ""
+    --     putStrLn ">>>>>Função de lista de adjacências aqui<<<<<\n"
+    --     putStrLn ""
+    --     main
+    --   "7" -> do
+    --     putStrLn ""
